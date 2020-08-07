69129667c0ea9777d03e19ce630bda056ff2d0db
Vũ Viết Phong <69337458+VVP-dot@users.noreply.github.com>
2020-08-07 17:22:21 +0700

    Update Exercise3.c

--- Exercise3.c
+++ Exercise3.c
@@ -8,9 +8,42 @@ Ex:
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int ex3(char *str){
-	//your codes here
+	int i, j, k, a, minString, maxString , max, min;
+	char substr[100][100];
+	
+	while( str[k] != '\0' ) {
+        	j = 0;
+        while( str[k] !=' '&&str[k] != '\0' ) {
+		substr[i][j] = str[k];
+            	k++;
+            	j++;
+        }
+        substr[i][j] = '\0';
+        	i++;
+        if( str[k] != '\0' ) {
+            k++;
+        }        
+    }
+    
+	int len = i;
+	max = strlen( substr[0] );
+	min = strlen( substr[0] );
+    
+	for( i = 0; i < len; i++ ) {
+    	a = strlen( substr[i] );
+    		if( a > max ) {
+			max = a;
+			maxString = i;
+		}
+    		if( a < min ) {
+    			min = a; 
+			minString = i;   
+		}
+    	}    
+ 	printf( "%s %s\n", substr[minString], substr[maxString] );
 }
 
 int main(int argc, char *argv[]) {
