e3a2cd19a2019f70ab96846d6d9550e51ec73616
Vũ Viết Phong <69337458+VVP-dot@users.noreply.github.com>
2020-08-07 16:47:36 +0700

    Update Exercise1.cpp

--- Exercise1.cpp
+++ Exercise1.cpp
@@ -12,7 +12,13 @@
 int ex1(int testcase){
 	int count;
 	int arr[5];
-	count = count(testcase);
+	int a;
+	
+	a = testcase;
+	while( a ){
+		a /= 10;
+		count++;
+	}
 	for( int i = 0; i < count; i++ ) {		
 		arr[i] = testcase % 10;
 		testcase /= 10;
@@ -155,14 +161,6 @@ int ex1(int testcase){
 			}
 		}	
 	}
-}
-int count(int n){
-	int count;
-	while( n ){
-		n /= 10;
-		count++;
-	}
-	return count;
 }
 
 int main(int argc, char *argv[]) {
