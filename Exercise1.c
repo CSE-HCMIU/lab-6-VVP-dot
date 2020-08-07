86c57431da53f70bebd62b15d02322f2189b299d
Vũ Viết Phong <69337458+VVP-dot@users.noreply.github.com>
2020-08-07 16:36:17 +0700

    Update Exercise1.c

--- Exercise1.c
+++ Exercise1.c
@@ -20,7 +20,7 @@ int ex1(int testcase, int count){
 	for( int i = count - 1; i >= 2; i-- ){
 		switch( arr[i] ) {  
 			case 0: 
-				printf("zero ");
+				printf("zero");
 				break;  
 			case 1:    
 				printf("one ");    
@@ -30,96 +30,129 @@ int ex1(int testcase, int count){
 				break;    
 			case 3:    
 				printf("three ");    
-				break;    
+					break;    
 			case 4:    
 				printf("four ");    
-				break;    
+					break;    
 			case 5:    
 				printf("five ");    
-				break;    
+					break;    
 			case 6:    
 				printf("six ");    
-				break;    
+					break;    
 			case 7:    
 				printf("seven ");    
-				break;    
+					break;    
 			case 8:    
 				printf("eight ");    
-				break;    
+					break;    
 			case 9:    
 				printf("nine ");    
-				break;    	 
+					break;    	 
 			}
-	if( i + 1 == 4 ) printf("thousand ");
-	if( i + 1 == 3 ) printf("hundred ");		 
-	}
+		if( i + 1 == 4 ) printf("thousand ");
+		if( i + 1 == 3 ) printf("hundred ");		 
+		}
 		
 	for( int i = count - 3; i > 0; i-- ){
-		switch( arr[i] ) {  
-			case 0: 
-				printf("zero");
-				break;  
-			case 1:    
-				printf("eleven ");    
-				break;    
-			case 2:    
-				printf("twelve ");    
-				break;    
-			case 3:    
-				printf("thirty ");    
-				break;    
-			case 4:    
-				printf("fourty ");    
-				break;    
-			case 5:    
-				printf("fifty ");    
-				break;    
-			case 6:    
-				printf("sixty ");    
-				break;    
-			case 7:    
-				printf("seventy");    
-				break;    
-			case 8:    
-				printf("eighty ");    
-				break;    
-			case 9:    
-				printf("ninety ");    
-				break;  
+		if( arr[1] != 1 ){
+			switch( arr[i] ) {  
+				case 0: 
+					printf("zero");
+					break;   
+				case 2:    
+					printf("twelve ");    
+					break;    
+				case 3:    
+					printf("thirty ");    
+					break;    
+				case 4:    
+					printf("fourty ");    
+					break;    
+				case 5:    
+					printf("fifty ");    
+					break;    
+				case 6:    
+					printf("sixty ");    
+					break;    
+				case 7:    
+					printf("seventy");    
+					break;    
+				case 8:    
+					printf("eighty ");    
+					break;    
+				case 9:    
+					printf("ninety ");    
+					break;  
+			}
+			switch( arr[0] ) {  
+				case 0: 
+					printf("zero");
+					break;  
+				case 1:    
+					printf("one");    
+					break;    
+				case 2:    
+					printf("two");    
+					break;    
+				case 3:    
+					printf("three");    
+					break;    
+				case 4:    
+					printf("four");    
+					break;    
+				case 5:    
+					printf("five");    	
+					break;    
+				case 6:    
+					printf("six");    
+					break;    
+				case 7:    
+					printf("seven");    
+	 				break;    
+				case 8:    
+					printf("eight");    
+					break;    
+				case 9:    
+					printf("nine");    
+					break;    	 
+			}
 		}
-		switch( arr[0] ) {  
-			case 0: 
-				printf("zero");
-				break;  
-			case 1:    
-				printf("one");    
-				break;    
-			case 2:    
-				printf("two");    
-				break;    
-			case 3:    
-				printf("three");    
-				break;    
-			case 4:    
-				printf("four");    
-				break;    
-			case 5:    
-				printf("five");    	
-				break;    
-			case 6:    
-				printf("six");    
-				break;    
-			case 7:    
-				printf("seven");    
-	 			break;    
-			case 8:    
-				printf("eight");    
-				break;    
-			case 9:    
-				printf("nine");    
-				break;    	 
-		}
-	}	
+		else {
+			switch( arr[1] ) {  
+				case 0: 
+					printf("ten");
+					break;  
+				case 1:    
+					printf("eleven");    
+					break;    
+				case 2:    
+					printf("twelve");    
+					break;    
+				case 3:    
+					printf("thirteen");    
+					break;    
+				case 4:    
+					printf("fourteen");    
+					break;    
+				case 5:    
+					printf("fifteen");    	
+					break;    
+				case 6:    
+					printf("sixteen");    
+					break;    
+				case 7:    
+					printf("seventeen");    
+	 				break;    
+				case 8:    
+					printf("eighteen");    
+					break;    
+				case 9:    
+					printf("nineteen");    
+					break;    	 
+			}
+		}	
+	}
 }
 int count(int n){
 	int count;
